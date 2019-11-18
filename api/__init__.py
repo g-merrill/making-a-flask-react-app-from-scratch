5983d3b59d59449d5d13a83e0cc8bce047cdf54a
Greg <gmerrill03@gmail.com>
2019-11-18 15:20:29 -0800

    Add database configurations to init file

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,15 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
   app = Flask(__name__)
+
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+  db.init_app(app)
 
   from .views import api
   app.register_blueprint(api)
