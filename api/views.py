eed43495fa79b84f59f9090c1adbc003a6bb1a38
Greg <gmerrill03@gmail.com>
2019-11-18 18:15:33 -0800

    Enter data into sqlite db via add_item post request

--- api/views.py
+++ api/views.py
@@ -1,9 +1,17 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from . import db
+from .models import Item
 
 api = Blueprint('api', __name__)
 
 @api.route('/add_item', methods=['POST'])
 def add_item():
+  item_data = request.get_json()
+
+  new_item = Item(name=item_data['name'], description=item_data['description'])
+
+  db.session.add(new_item)
+  db.session.commit()
 
   return 'Done', 201
 
@@ -13,3 +21,6 @@ def items():
   items = []
 
   return jsonify({ 'items': items })
+
+# name = db.Column(db.String(50))
+# description = db.Column(db.String(250))
