faa3663dc27c4e5e311aa94df34290e4e2957835
Greg <gmerrill03@gmail.com>
2019-11-18 19:42:42 -0800

    Add react frontend

---
+++ react-frontend/src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { Items } from './components/Items';
+import { ItemForm } from './components/ItemForm';
+import { Container } from 'semantic-ui-react';
+
+function App() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('/items').then(res => 
+      res.json().then(data => {
+        setItems(data.items)
+      })
+    );
+  }, []);
+
+  return (
+    <Container 
+      style={{ marginTop: 40 }}
+    >
+      <ItemForm 
+        onNewItem={item => setItems(currentItems => [item, ...currentItems])} />
+      <Items 
+        items={ items }
+      />
+    </Container>
+  );
+}
+
+export default App;
